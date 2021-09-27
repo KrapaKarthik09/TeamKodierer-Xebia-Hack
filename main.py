7e7d4b26e717bc6a7686b58708e0b55096b3d855
Arun-cat <71755621+Arun-cat@users.noreply.github.com>
2021-09-27 23:39:21 +0530

    Update main.py
    Second Commit

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 import pandas as pd
-import numpy as np
 import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+st.title('Carbon Footprint Tracker')
+data = pd.read_csv('carbon.csv')
+st.sidebar.title("Dashboard")
 
-st.write("""
-### Carbon Footprint Tracker by Team Kodierer
-""")
+st.sidebar.title("Visualization Selector")
+select = st.sidebar.selectbox('Visualization type', ['Bar plot', 'pie chart'], key='1')
 
-st.sidebar.header('User Input Features')
+st.sidebar.title("Display period")
+select1 = st.sidebar.selectbox('Period', ['Daily', 'Monthly','Yearly'], key='1')
 
+submit = st.sidebar.button('Submit')
+
+if submit:
+    if select1 == 'Daily':
+        if select == 'pie chart':
+            fig = px.pie(data, values=data['GHG emissions (kg CO2) / year'][:], names=data['Item'][:], title='Total Carbon emission')
+            st.plotly_chart(fig)
+        if select == 'Bar plot':
+            fig = go.Figure(data=[go.Bar(name='Item', x=data['Energy use (kWh/ year)'][:], y=data['GHG emissions (kg CO2) / year'][:])])
+            st.plotly_chart(fig)
+    if select1 == 'Monthly':
+        if select == 'pie chart':
+            fig = px.pie(data, values=data['GHG emissions (kg CO2) / year'][:], names=data['Item'][:], title='Total Carbon emission')
+            st.plotly_chart(fig)
+        if select == 'Bar plot':
+            fig = go.Figure(data=[go.Bar(name='Item', x=data['Energy use (kWh/ year)'][:], y=data['GHG emissions (kg CO2) / year'][:])])
+            st.plotly_chart(fig)
+    if select1 == 'Yearly':
+        if select == 'pie chart':
+            fig = px.pie(data, values=data['GHG emissions (kg CO2) / year'][:], names=data['Item'][:], title='Total Carbon emission')
+            st.plotly_chart(fig)
+        if select == 'Bar plot':
+            fig = go.Figure(data=[go.Bar(name='Item', x=data['Energy use (kWh/ year)'][:], y=data['GHG emissions (kg CO2) / year'][:])])
+            st.plotly_chart(fig)
 
